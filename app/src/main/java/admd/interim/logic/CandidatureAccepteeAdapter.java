9a3bb4987d3cb92a797b07857106c301d68120c0
Adam <adam22daia11@gmail.com>
2024-05-16 10:34:16 +0200

    Employeur : consultation des candidature accept avec aff canddiat +candidature

--- app/src/main/java/admd/interim/logic/CandidatureAccepteeAdapter.java
+++ app/src/main/java/admd/interim/logic/CandidatureAccepteeAdapter.java
@@ -47,13 +47,16 @@ public class CandidatureAccepteeAdapter extends RecyclerView.Adapter<Candidature
 
         holder.buttonConsulter.setOnClickListener(v -> {
             Intent intent = new Intent(context, DetailsCandidatActivityEmployeur.class);
-            intent.putExtra("NOM", candidature.getNomCandidat());
-            intent.putExtra("PRENOM", candidature.getPrenomCandidat());
-            intent.putExtra("EMAIL", candidature.getEmailCandidat());
-            //intent.putExtra("NUMERO_TELEPHONE", candidature.getNumeroTelephone());
-            //intent.putExtra("DATE_NAISSANCE", candidature.getDateNaissance());
-            //intent.putExtra("NATIONALITE", candidature.getNationalite());
-            //intent.putExtra("VILLE", candidature.getVille());
+            Candidat candidat = databaseHelper.getCandidatByID((int)candidature.getIdCandidat());
+            if (candidat != null) {
+                intent.putExtra("NOM", candidat.getNom());
+                intent.putExtra("PRENOM", candidat.getPrenom());
+                intent.putExtra("EMAIL", candidat.getEmail());
+                intent.putExtra("NUMERO_TELEPHONE", candidat.getNumeroTelephone());
+                intent.putExtra("DATE_NAISSANCE", candidat.getDateNaissance());
+                intent.putExtra("NATIONALITE", candidat.getNationalite());
+                intent.putExtra("VILLE", candidat.getVille());
+            }
             intent.putExtra("CV", candidature.getCvCandidat());
 
             Offre offre = databaseHelper.getOffreById(candidature.getIdOffre());
@@ -74,31 +77,35 @@ public class CandidatureAccepteeAdapter extends RecyclerView.Adapter<Candidature
             String[] options = {"Email", "Téléphone", "SMS"};
 
             builder.setItems(options, (dialog, which) -> {
-                switch (which) {
-                    case 0:
-                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-                        emailIntent.setType("text/plain");
-                        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{candidature.getEmailCandidat()});
-                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Sujet de votre email");
-                        context.startActivity(Intent.createChooser(emailIntent, "Choisir une application pour envoyer l'email"));
-                        break;
-                    case 1:
-                        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
-                        //phoneIntent.setData(Uri.parse("tel:" + candidature.getNumeroTelephone()));
-                        context.startActivity(phoneIntent);
-                        break;
-                    case 2:
-                        Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
-                        //smsIntent.setData(Uri.parse("smsto:" + candidature.getNumeroTelephone()));
-                        smsIntent.putExtra("sms_body", "Message SMS à envoyer");
-                        context.startActivity(smsIntent);
-                        break;
+                Candidat candidat = databaseHelper.getCandidatByID((int)candidature.getIdCandidat());
+                if (candidat != null) {
+                    switch (which) {
+                        case 0:
+                            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                            emailIntent.setType("text/plain");
+                            emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{candidat.getEmail()});
+                            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Sujet de votre email");
+                            context.startActivity(Intent.createChooser(emailIntent, "Choisir une application pour envoyer l'email"));
+                            break;
+                        case 1:
+                            Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                            phoneIntent.setData(Uri.parse("tel:" + candidat.getNumeroTelephone()));
+                            context.startActivity(phoneIntent);
+                            break;
+                        case 2:
+                            Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+                            smsIntent.setData(Uri.parse("smsto:" + candidat.getNumeroTelephone()));
+                            smsIntent.putExtra("sms_body", "Message SMS à envoyer");
+                            context.startActivity(smsIntent);
+                            break;
+                    }
                 }
             });
 
             builder.show();
         });
     }
+
 
     @Override
     public int getItemCount() {
